a8dc0d3f7defffb350f9226ed4b9ae73aa6c3fa5
Jordan <jordyeng@gmail.com>
2016-02-12 08:07:13 -0800

    Fixed things

--- src/TestMusic.java
+++ src/TestMusic.java
@@ -1,42 +1,51 @@
-/**
- * Created by ros_jheng on 1/22/2016.
- */
 
 import java.io.*;
 import javax.sound.sampled.*;
 import java.nio.ByteBuffer;
-import java.util.Random;
 
 public class TestMusic {
     public static void main(String[] args) throws UnsupportedAudioFileException, IOException{
         ReadingWAV audioRead = new ReadingWAV(new File("done.wav"));
-       // AudioFormat audioFormat = audioRead.getFormat();
+        // AudioFormat audioFormat = audioRead.getFormat();
         byte[] timeFrequencyArrayX = audioRead.toByteArray();
-
         testFft(timeFrequencyArrayX);
     }
 
     private static void testFft(byte[] timeFrequencyArrayX) {
+        int arraySize = 32768;
+        double[] inputImag = new double[arraySize];
+        double[] inputReal = new double[arraySize];
 
-        // need to change these so my arrays are used
-        double[] wavReals = toDoubleArray(timeFrequencyArrayX);
-        int size = wavReals.length;
-
-        double[] inputreal = wavReals;
-            double[] inputimag = wavReals;
-
-            double[] refoutreal = new double[size];
-            double[] refoutimag = new double[size];
-
-            double[] newArray = FFT.fft(inputreal, inputimag, false);
-
-            System.out.println(newArray.toString());
+        for(int i = 0; i < arraySize; i++){
+            inputReal[i] = 0.0;
+            inputImag[i] = 0.0;
         }
 
+        for(int i = 0; i < toDoubleArray(timeFrequencyArrayX).length; i++){
+            inputImag[i] = toDoubleArray(timeFrequencyArrayX)[i];
+        }
+
+        double[] newArray = FFT.fft(inputReal, inputImag, true);
+
+        // make it transform new array the other to see if it works.
+        for(double i: newArray) {
+            System.out.println(i);
+        }
+
+        double[] meow = FFT.fft(inputReal, newArray, false);
+        if (inputImag == meow){
+            System.out.println();
+            System.out.println("yay");
+        } else{
+            System.out.println("boo");
+        }
+    }
+
+    // This isn't converting the array exactly right. :(
     public static double[] toDoubleArray(byte[] byteArray){
         int times = Double.SIZE / Byte.SIZE;
         double[] doubles = new double[byteArray.length / times];
-        for(int i=0;i<doubles.length;i++){
+        for(int i = 0; i < doubles.length; i++){
             doubles[i] = ByteBuffer.wrap(byteArray, i*times, times).getDouble();
         }
         return doubles;
